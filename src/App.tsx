26e8763a4fd623763e9a762f877d3d58512a7277
Zenzyde <embi4882@gmail.com>
2023-08-02 16:05:25 +0200

    Visual update for mobile (and hopefully fetch-click on mobile will work properly now)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import QuoteCard from './components/QuoteCard';
 import TitleCard from './components/TitleCard';
@@ -16,8 +16,8 @@ function App()
 	
 	const handleFetchNewQuote = async () =>
 	{
-		const endpoint = "https://api.adviceslip.com/advice"; //"http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&origin=*"; //"https://api.quotable.io/random";
-		await fetch(endpoint).then(response => response.json()).then((data) => setData(data as Quote)).then((log) => console.log(log)).catch(error => console.log(error));
+		const endpoint = "https://api.adviceslip.com/advice";
+		await fetch(endpoint).then(response => response.json()).then((data) => setData(data as Quote)).catch(error => console.log(error));
 	}
 	
 	useEffect(() => {
@@ -25,10 +25,10 @@ function App()
 	}, []);
 	
 	return (
-		<div className="App bg-blue-300 h-full w-full absolute">
+		<div className="App bg-gradient-to-tr from-cyan-500 to-blue-300 h-full w-full absolute">
 			<TitleCard/>
 			{data && <QuoteCard slip={data.slip}  />}
-			<button className='p-2 sm:text-sm md:text-base bg-black text-white border-solid rounded-[55px] sm:top-[85%] md:top-3/4 -translate-x-1/2 hover:translate-y-1 absolute cursor-pointer hover:shadow-[0_3px_5px_1px_rgb(17,24,39)] shadow-lg shadow-gray-900' onMouseUp={handleFetchNewQuote}>Click to fetch new advice</button>		
+			<button className='p-2 sm:text-sm md:text-base bg-blue-600 text-white rounded-[55px] top-[85%] -translate-x-1/2 absolute cursor-pointer hover:bg-blue-700 active:bg-blue-800' onMouseUp={handleFetchNewQuote} onTouchEnd={handleFetchNewQuote} >Click to fetch new advice</button>		
 		</div>
 	);
 }
--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -3,7 +3,7 @@ import { Quote } from "../App";
 
 const QuoteCard = ({ slip }: Quote) => (
 	<div className="bg-blue-800 p-2 border-solid rounded-[55px] top-1/2 md:top-[45%] left-1/2 absolute -translate-x-1/2 shadow-lg shadow-gray-900">
-		<p className="p-2 sm:text-base md:text-4xl text-zinc-100 border-solid rounded-[55px]">{slip.advice}</p>
+		<p className="p-2 sm:text-2xl md:text-4xl text-zinc-100 border-solid rounded-[55px]">{slip.advice}</p>
 	</div>
 );
 
